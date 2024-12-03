31c5e63d33ede4fdf0e361b0506e7bdd100e2bce
MaciejKatychow <maciejkatychow@gmail.com>
2024-12-03 23:22:41 +0100

    day3 part2

--- day3/main.py
+++ day3/main.py
@@ -10,8 +10,20 @@ def calculate_mul(mul: str) -> int:
     return a
 
 outcome = 0
+count_do = 0
+count_dont = 0
+do = True
 for row in data:
-    good_muls = re.findall(r'mul\(\d+,\d+\)', row)
-    for mul in good_muls:
-        outcome += calculate_mul(mul)
+    good_muls = re.findall(r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)', row)
+    count_do += good_muls.count("do()")
+    count_dont += good_muls.count("don't()")
+    for index, mul in enumerate(good_muls):
+        if mul == "do()":
+            do = True
+        elif mul == "don't()":
+            do = False
+        else:
+            if do:
+                digits = re.findall(r'\d+', mul)
+                outcome += int(digits[0]) * int(digits[1])
 print(outcome)
