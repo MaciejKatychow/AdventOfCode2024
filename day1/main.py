f1294bc5f54373cc405162b67dcf87a6262f65da
MaciejKatychow <maciejkatychow@gmail.com>
2024-12-03 10:35:11 +0100

    task 1

---
+++ day1/main.py
@@ -0,0 +1,19 @@
+with open ('day1/input.txt') as f:
+    data = f.read().splitlines()
+
+
+list_1 = []
+list_2 = []
+for row in data:
+    parts = row.split(" ")
+    list_1.append(int(parts[0]))
+    list_2.append(int(parts[-1]))
+
+sorted_list_1 = sorted(list_1)
+sorted_list_2 = sorted(list_2)
+
+outcome = 0
+for i in range(len(sorted_list_1)):
+    outcome += abs(sorted_list_1[i] - sorted_list_2[i])
+
+print(outcome)
